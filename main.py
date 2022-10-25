e955f0b4588760d230e7dec4943e9b4c0ce9be94
skori <skorik.roma@gmail.com>
2022-10-25 23:22:18 +0900

    wdQADW

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 n, m = map(int, input().split())
 w = [[1] * m for i in range(n)]
-for i in range(1, n):
-    for j in range(1, m):
-        w[i][j] = w[i][j - 1]
+for i ] = w[i][j - 1]
 print(w[-1][-1])
