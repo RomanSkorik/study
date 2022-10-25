8690bc812661aa9ead69c4019ffe1b934fc6d7ae
skori <skorik.roma@gmail.com>
2022-10-25 23:11:58 +0900

    mope

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ n, m = map(int, input().split())
 w = [[1] * m for i in range(n)]
 for i in range(1, n):
     for j in range(1, m):
-        w[i][j] = w[i][j - 1] + w[i - 1][j]
+        w[i][j] = w[i][j - 1]
 print(w[-1][-1])
